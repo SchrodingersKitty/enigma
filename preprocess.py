d3a4962745c0944defa8e6da32384124eb226f44
SchrodingersKitty <radium213@gmail.com>
2018-04-27 23:00:44 +0300

    Fixed indexing exception. Changed preprocessing a little.

--- preprocess.py
+++ preprocess.py
@@ -3,14 +3,14 @@ import sys
 
 def preprocess(infile, outfile):
     #punctuation = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    punctuation = "!\"#$%&()*,/:;<>?@[\]^_`{|}~"
+    punctuation = "!\"#$%&()*,/;?@[\]^_`{|}~"
     strip_punctuation = str.maketrans('', '', punctuation)
     newline_punctuation = "."
     with open(infile, "r", encoding="utf-8") as f:
         with open(outfile, "w", encoding="utf-8") as s:
             for line in f:
                 line = re.sub(r'https?:\S+', '', line)
-                line = re.sub(r'<:\S+>', '', line)
+                #line = re.sub(r'<:\S+>', '', line)
                 line = line.translate(strip_punctuation)
                 #line = line.lower()
                 lines = line.split(newline_punctuation)
